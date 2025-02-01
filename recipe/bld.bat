b19b8c00222bdf09c1e0cb7424474af55dddf4e9
Daniel Nachun <daniel.nachun@gmail.com>
2025-01-31 16:38:05 -0800

    fix UCRT migration

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,20 +1,8 @@
-:: Install cargo-license
-set CARGO_HOME=%BUILD_PREFIX%\cargo
-mkdir %CARGO_HOME%
-icacls %CARGO_HOME% /grant Users:F
-cargo install cargo-bundle-licenses
 :: Check that all downstream libraries licenses are present
-set PATH=%PATH%;%CARGO_HOME%\bin
-cargo bundle-licenses --format yaml --output CI.THIRDPARTY.yml --previous THIRDPARTY.yml --check-previous
+cargo bundle-licenses --format yaml --output THIRDPARTY.yml
 
 :: build
-cargo install --locked --root "%PREFIX%" --path . || goto :error
-
-:: strip debug symbols
-strip "%PREFIX%\bin\crabz.exe" || goto :error
-
-:: remove extra build file
-del /F /Q "%PREFIX%\.crates.toml"
+cargo install --bins --no-track --locked --root "%PREFIX%" --path . || goto :error
 
 goto :EOF
 
